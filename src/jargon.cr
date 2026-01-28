583cd0daa3a654c32a5c9c0c35bf9729990468aa
Thomas Sawyer <transfire@gmail.com>
2026-01-28 00:16:40 -0500

    Bump version to 0.11.0

--- src/jargon.cr
+++ src/jargon.cr
@@ -4,7 +4,7 @@ require "./jargon/result"
 require "./jargon/completion"
 
 module Jargon
-  VERSION = "0.10.0"
+  VERSION = "0.11.0"
 
   class ParseError < Exception
     getter errors : Array(String)
